b0d8be884d366a8352495f3e174293d5b249f7d1
Gonzalo <gonzalomedinamedina@gmail.com>
2023-05-09 19:28:31 +0200

    Basic call to dataProvider to  get all clients from mocky.

--- src/Controllers/clientsController.js
+++ src/Controllers/clientsController.js
@@ -1,7 +1,11 @@
+import { fetchData } from '../dataProvider.js';
+const clientsURL = 'https://www.mocky.io/v2/5808862710000087232b75ac';
 
-export function registerClientsEndPoints(app)
+export async function registerClientsEndPoints(app)
 {
-  app.get('/clients', (req, res) => {
-    res.send('first client endpoint');
+  app.get('/clients', async (req, res) => 
+  {
+    const clients = await fetchData(clientsURL);
+    res.send('first client endpoint ' + clients);
   });
 }
